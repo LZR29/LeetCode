4eaff7bc9d56513d84ceb92c3c8784a078f976b3
linzerong <564952475@qq.com>
2018-12-07 15:27:22 +0800

    leetcode

--- src/easy/No14.java
+++ src/easy/No14.java
@@ -3,12 +3,14 @@ package easy;
 public class No14 {
 
 	public static String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0)
-        	return "";
+        if(strs.length == 0) {
+			return "";
+		}
 		String res = strs[0];
 		for (int i = 1; i < strs.length;) {
-			if(strs[i].indexOf(res) != 0 && res.length()!=0)
+			if(strs[i].indexOf(res) != 0 && res.length()!=0) {
 				res = res.substring(0, res.length() - 1);
+			}
 			else {
 				 i++;
 			}
