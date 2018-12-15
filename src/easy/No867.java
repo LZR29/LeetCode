4c334258ebebfb481509c2f074bf298a7e7285b1
linzerong <564952475@qq.com>
2018-12-15 10:20:14 +0800

    easy

---
+++ src/easy/No867.java
@@ -0,0 +1,20 @@
+package easy;
+
+/**
+ * @authoor linzerong
+ * @create 2018-12-15 10:16
+ */
+public class No867 {
+
+    public int[][] transpose(int[][] A) {
+        int row = A.length;
+        int col = A[0].length;
+        int[][] ans = new int[col][row];
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                ans[i][j] = A[j][i];
+            }
+        }
+        return ans;
+    }
+}
