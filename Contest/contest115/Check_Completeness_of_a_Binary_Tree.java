7feb6b5fa6b302801813637d4e27ae6a701878b7
linzerong <564952475@qq.com>
2018-12-16 12:32:18 +0800

    queue接口与linkedlist的学习还有待提高

---
+++ Contest/contest115/Check_Completeness_of_a_Binary_Tree.java
@@ -0,0 +1,42 @@
+package contest115;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @authoor linzerong
+ * @create 2018-12-16 10:32
+ */
+public class Check_Completeness_of_a_Binary_Tree {
+    public boolean isCompleteTree(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        boolean ans = true;
+        boolean isNode = false;
+     //   ((LinkedList<TreeNode>) q).push(root);
+        q.add(root);
+        while (!q.isEmpty()){
+            TreeNode node = q.poll();
+            if(isNode){
+                if(node.left != null || node.right != null){
+                    ans = false;
+                    break;
+                }
+            }else {
+                if (node.left != null && node.right != null) {
+                    q.add(node.left);
+                    q.add(node.right);
+                } else if (node.left == null && node.right != null) {
+                    ans = false;
+                    break;
+                } else if ((node.left != null && node.right == null)) {
+                    q.add(node.left);
+                    isNode = true;
+                }else {
+                    isNode = true;
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ Contest/contest115/TreeNode.java
@@ -0,0 +1,12 @@
+package contest115;
+
+/**
+ * @authoor linzerong
+ * @create 2018-12-16 10:33
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
