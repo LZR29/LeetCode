5d36798540385ea8737cdbf10cdee380614e7f89
linzerong <564952475@qq.com>
2018-12-23 11:15:49 +0800

    周赛116

---
+++ Contest/contest116/Maximum_Width_Ramp.java
@@ -0,0 +1,21 @@
+package contest116;
+
+/**
+ * @authoor linzerong
+ * @create 2018-12-23 10:43
+ */
+public class Maximum_Width_Ramp {
+    public int maxWidthRamp(int[] A) {
+        int len = A.length;
+        int max = 0;
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if(A[j] >= A[i]){
+                    max = Math.max(j - i, max);
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
---
+++ Contest/contest116/N_Repeated_Element_in_Size_2N_Array.java
@@ -0,0 +1,22 @@
+package contest116;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @authoor linzerong
+ * @create 2018-12-23 10:35
+ */
+public class N_Repeated_Element_in_Size_2N_Array {
+    public int repeatedNTimes(int[] A) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            if(!set.contains(A[i])){
+                set.add(A[i]);
+            }else {
+                return A[i];
+            }
+        }
+        return 0;
+    }
+}
