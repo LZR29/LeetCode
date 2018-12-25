5fb62d83db5d332c1163ef7c86e3c12df13dd855
linzerong <564952475@qq.com>
2018-12-25 20:12:02 +0800

    用两个指针一个走一步，一个走两步

---
+++ src/easy/No876.java
@@ -0,0 +1,20 @@
+package easy;
+
+/**
+ * @authoor linzerong
+ * @create 2018-12-25 20:05
+ */
+public class No876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast.next != null){
+            return slow.next;
+        }
+        return slow;
+    }
+}
