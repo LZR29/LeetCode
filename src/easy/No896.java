e40759dca38338fefc20cebadd4a86a7b2b9cf2e
linzerong <564952475@qq.com>
2018-12-26 10:19:15 +0800

    easy

---
+++ src/easy/No896.java
@@ -0,0 +1,29 @@
+package easy;
+
+/**
+ * @authoor linzerong
+ * @create 2018-12-26 10:00
+ */
+public class No896 {
+    public boolean isMonotonic(int[] A) {
+        if(A.length == 1) {
+            return true;
+        }
+        for (int i = 1; i < A.length; i++) {
+            if(A[i-1] > A[i]){
+                for (int j = i+1; j < A.length; j++) {
+                    if(A[j-1] < A[j]){
+                        return false;
+                    }
+                }
+            } else if(A[i-1] < A[i]){
+                for (int j = i+1; j < A.length; j++) {
+                    if(A[j-1] > A[j]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
