69a74cd691cec83078d68e8649b53db7fd9b6521
linzerong <564952475@qq.com>
2019-01-02 18:33:21 +0800

    easy

---
+++ src/easy/No821.java
@@ -0,0 +1,32 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @authoor linzerong
+ * @create 2019-01-02 18:27
+ */
+public class No821 {
+    public int[] shortestToChar(String S, char C) {
+        int len = S.length();
+        int[] ans = new int[len];
+        List<Integer> pos = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            ans[i] = len;
+            if(C == S.charAt(i)){
+                pos.add(i);
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if(S.charAt(i) == C){
+                ans[i] = 0;
+            }else {
+                for(int j : pos){
+                    ans[i] = Math.min(Math.abs(i - j),ans[i]);
+                }
+            }
+        }
+        return ans;
+    }
+}
