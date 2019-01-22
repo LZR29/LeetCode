faabac50278f7b0e1b9594e724608dfaa2a62000
linzerong <564952475@qq.com>
2019-01-22 22:03:56 +0800

    easy

---
+++ src/easy/No977.java
@@ -0,0 +1,17 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * @authoor linzerong
+ * @create 2019-01-22 21:58
+ */
+public class No977 {
+    public int[] sortedSquares(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            A[i] *= A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
